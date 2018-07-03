4aa2570e3977296cced8c49931222681d43cf485
Milosz Nowicki <milosz.nowicki@edpauto.com>
2018-07-03 15:29:57 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,13 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextCssPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const srcPath = path.join(__dirname, '/../src');
 
 module.exports = {
     mode: "development",
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        port: 8000,
+        port: 4200,
     },
     plugins: [
         new MiniCssExtractPlugin({
